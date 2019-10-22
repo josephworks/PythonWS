2938728adfbd5251f057c1ade3cb8b3ce54c9485
Joseph Robinson <smartech56@gmail.com>
2019-10-22 14:53:37 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PythonWS',
+    version='v1.0',
+    packages=[''],
+    url='https://github.com/josephworks/PythonWS',
+    license='MIT',
+    author='Joseph Robinson',
+    author_email='smartech56@gmail.com',
+    description='My Python workspace! Feel free to PR!'
+)
