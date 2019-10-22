8e4b96ebdb9c91b12b92808b2a25b729e0b517fa
Joseph Robinson <smartech56@gmail.com>
2019-10-22 15:26:18 -0700

    Formatted factor

--- factor.py
+++ factor.py
@@ -3,7 +3,7 @@
 for n in range(1, 100000):
     for x in range(2, n):
         if n % x == 0:
-            print(n, 'equals', x, '*', n//x)
+            print(n, 'equals', x, '*', n // x)
             break
     else:
         # loop fell through without finding a factor
